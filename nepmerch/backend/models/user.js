197ea4540285c0ded96e48c3ebc1793465e2bf1c
CohleM <manish981194@gmail.com>
2021-04-24 21:22:04 +0545

    Two database clusters connected

---
+++ nepmerch/backend/models/user.js
@@ -0,0 +1,44 @@
+
+//const conn1 = require('../server.js');
+const mongoose = require("mongoose");
+const {userConnection, newConnection} = require('../connections');
+const { array } = require("prop-types");
+const userSchema = new mongoose.Schema({
+	name: {
+		type: String,
+		require: true,
+		min: 6,
+		max: 255,
+	},
+
+	email: {
+		type: String,
+		require: true,
+		min: 6,
+		max: 255,
+	},
+
+	password: {
+		type: String,
+		require: true,
+		min: 6,
+		max: 1024,
+	},
+	date: {
+		type: Date,
+		default: Date.now,
+	},
+	cart: {
+		type: Array,
+		default: [],
+	},
+	history: {
+		type: Array,
+		default: [],
+	},
+});
+
+
+
+const User  = newConnection.model('User',userSchema)
+module.exports = User;
